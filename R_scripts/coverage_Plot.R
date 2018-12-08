d7ca49b6dabc392bf3d2c8ff94216d4e3d096804
Jose F. Sanchez <jfsanchezherrero@ub.edu>
2018-12-08 18:49:20 +0100

    Link submodule Functional annotation

--- R_scripts/coverage_Plot.R
+++ R_scripts/coverage_Plot.R
@@ -1,7 +1,7 @@
 
 
 setwd("/Users/jfsh/GIT_REPOS/Dysdera_silvatica_genome")
-genomeSize = 1700000000
+genomeSize = 1360000000
 ## PE_freq
 PE_freq <-read.csv(file="R_scripts/coverage_Stats/PE_Reads_sorted.coverage.freq", sep="\t", header=TRUE)
 
@@ -24,45 +24,44 @@ ALL_reads <-read.csv(file="R_scripts/coverage_Stats/merge_sorted.coverage.freq",
 ## Plot
 layout(respect = TRUE, matrix(c(1,1), 1, 1, byrow = TRUE), widths=c(1,1), heights=c(1,1))
 
-plot(Pacbio_ALL_Reads$X..Coverage[2:100],Pacbio_ALL_Reads$Count[2:100], type="l", col="blue", lty=1, xlab="Coverage", ylab="Frequency")
-lines(MP_ALL_Reads$X..Coverage[2:100],MP_ALL_Reads$Count[2:100], type="l", col="red")
-lines(ALL_reads$X..Coverage[2:100],ALL_reads$Count[2:100], type="l", col="black")
-lines(PE_freq$X..Coverage[2:100],PE_freq$Count[2:100], type="l", col="orange")
-lines(Nanopore_ALL_Reads$X..Coverage[2:100],Nanopore_ALL_Reads$Count[2:100], type="l", col="green")
+plot(Pacbio_ALL_Reads$X..Coverage[2:100],Pacbio_ALL_Reads$Count[2:100], type="l", lwd=2, col="orange", lty=1, xlab="Coverage", ylab="Frequency")
+lines(MP_ALL_Reads$X..Coverage[2:100],MP_ALL_Reads$Count[2:100], type="l", lwd=2, col="red")
+lines(ALL_reads$X..Coverage[2:100],ALL_reads$Count[2:100], type="l", lwd=2, col="black")
+lines(PE_freq$X..Coverage[2:100],PE_freq$Count[2:100], type="l", lwd=2, col="blue")
+lines(Nanopore_ALL_Reads$X..Coverage[2:100],Nanopore_ALL_Reads$Count[2:100], type="l", lwd=2, col="dark green")
 
 #lines(Pacbio_ALL_Reads$X..Coverage[2:100],Pacbio_ALL_Reads$Count[2:100], type="l", col="green")
 
 # ALL
 sumCoverage_all <- sum(as.numeric( ALL_reads[0:8079,1] * ALL_reads[0:8079,2]))
-abline(v=sumCoverage_all/genomeSize,lty=2,col="black")
-sumCoverage_all/genomeSize
+mean_all <- sumCoverage_all/genomeSize
+abline(v=mean_all,lty=2, col="black")
 
 # PE
 sumCoverage_PE <- sum(as.numeric( PE_freq[0:8079,1] * PE_freq[0:8079,2]))
-abline(v=sumCoverage_PE/genomeSize,lty=2,col="orange")
-sumCoverage_PE/genomeSize
+mean_PE <- sumCoverage_PE/genomeSize
+abline(v=mean_PE,lty=2, col="blue")
 
 # MP
 sumCoverage_MP <- sum(as.numeric( MP_ALL_Reads[0:8059,1] * MP_ALL_Reads[0:8059,2]))
-abline(v=sumCoverage_MP/genomeSize,lty=2,col="red")
-sumCoverage_MP/genomeSize
+mean_MP <- sumCoverage_MP/genomeSize
+abline(v=mean_MP,lty=2, col="red")
 
-# MP
+# Pacbio
 sumCoverage_PB <- sum(as.numeric( Pacbio_ALL_Reads[0:289,1] * Pacbio_ALL_Reads[0:289,2]))
-abline(v=sumCoverage_PB/genomeSize,lty=2,col="blue")
-sumCoverage_PB/genomeSize
+mean_PB <- sumCoverage_PB/genomeSize
+abline(v=mean_PB,lty=2, col="orange")
 
-# MP
+# Nanopore
 sumCoverage_Nano <- sum(as.numeric( Nanopore_ALL_Reads[0:8047,1] * Nanopore_ALL_Reads[0:8047,2]))
-abline(v=sumCoverage_Nano/genomeSize,lty=2,col="green")
-sumCoverage_Nano/genomeSize
-
+mean_Nano <- sumCoverage_Nano/genomeSize
+abline(v=mean_Nano,lty=2, col="dark green")
 
 legend(
   "topright", ## POSITION 
-  c("All reads coverage","Mean coverage", "PE coverage", "Mean Coverage PE", "MP coverage", "Mean Coverage MP"), # puts text in the legend 
-  lty=c(1,2,1,2,1,2),               # gives the legend appropriate symbols (lines)
-  lwd=c(3,3,3,3,3,3), ## width
-  col=c("black","black","blue","blue","red","red") # gives the legend lines the correct color
+  c("All reads coverage","Mean coverage", "PE coverage", "Mean Coverage PE", "MP coverage", "Mean Coverage MP", "Pacbio Coverage", "Mean Pacbio Coverage", "Nanopore Coverage", "Mean Nanopore Coverage" ), # puts text in the legend 
+  lty=c(1,2,1,2,1,2,1,2,1,2),               # gives the legend appropriate symbols (lines)
+  lwd=c(3,1,3,1,3,1,3,1,3,1), ## width
+  col=c("black","black","blue","blue","red","red", "orange", "orange", "dark green", "dark green") # gives the legend lines the correct color
 )
 
